8ea1b97ac18fb9097913af54e65bb454ed619ae1
Rxzh <pierre.belamri-regenpied@mines-paristech.fr>
2021-02-07 22:47:15 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Image,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import DocumentScanner from 'react-native-document-scanner';
+
+function App (){
+
+  return (<View></View>
+
+  );
+};
+
+
+export default App;
