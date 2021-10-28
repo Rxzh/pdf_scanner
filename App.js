1968affad3b5177dd9e2ba3afe7cb7281f1f1b9c
Rxzh <pierre.belamri-regenpied@mines-paristech.fr>
2021-10-28 10:50:13 +0200

    update

--- App.js
+++ App.js
@@ -12,9 +12,18 @@ import {
 import DocumentScanner from 'react-native-document-scanner';
 
 function App (){
-
-  return (<View></View>
-
+  return (
+    <View>
+    <DocumentScanner
+      //style={styles.scanner}
+      //onPictureTaken={handleOnPictureTaken}
+      overlayColor="rgba(255,130,0, 0.7)"
+      enableTorch={false}
+      quality={0.5}
+      detectionCountBeforeCapture={5}
+      detectionRefreshRateInMS={50}
+    />
+  </View>
   );
 };
 
